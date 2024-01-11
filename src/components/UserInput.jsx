1b619c430b0514287488271769839160dbd5fda6
Alejandro Armas <lxarmas@gmail.com>
2024-01-11 10:19:52 -0800

    InvestmentResults_added

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,4 +1,4 @@
-export default function UserInput(onChange, UserInput) {
+export default function UserInput({onChange, userInput}) {
     return (
         <section id='user-input'>
             <div className="input-group">
@@ -7,7 +7,7 @@ export default function UserInput(onChange, UserInput) {
                     <input
                         type="number"
                         required
-                        value={UserInput.initialInvestment}
+                        value={userInput.initialInvestment}
                         onChange={( event ) =>
                         onChange( 'initialInvestment', event.target.value )
                     }
@@ -20,7 +20,7 @@ export default function UserInput(onChange, UserInput) {
                     <label>Annual Investment</label>
                     <input type="number"
                         required
-                        value={UserInput.annualInvestment}
+                        value={userInput.annualInvestment}
                     onChange={( event ) => 
                     onChange( 'annualInvestment', event.target.value )
                         }
@@ -33,7 +33,7 @@ export default function UserInput(onChange, UserInput) {
                     <label>Expected Return</label>
                     <input type="number"
                     required 
-                    value={UserInput.expectedReturn}
+                    value={userInput.expectedReturn}
                     onChange={( event ) => 
                     onChange( 'expectedReturn', event.target.value )
                     }
@@ -46,7 +46,7 @@ export default function UserInput(onChange, UserInput) {
                     <label>Duration</label>
                     <input type="number"
                     required 
-                    value={UserInput.duration}
+                    value={userInput.duration}
                     onChange={( event ) =>
                     onChange('duration', event.target.value)}
                     />
